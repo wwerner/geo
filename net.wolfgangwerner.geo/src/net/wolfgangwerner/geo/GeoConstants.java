ccb5f64d997a8323176db648d01bd21c86c41fd6
Wolfgang Werner <wolfgang.werner@gmail.com>
2010-03-04 00:35:20 +0100

    added great circle algorithm

--- net.wolfgangwerner.geo/src/net/wolfgangwerner/geo/GeoConstants.java
+++ net.wolfgangwerner.geo/src/net/wolfgangwerner/geo/GeoConstants.java
@@ -1,12 +1,14 @@
 package net.wolfgangwerner.geo;
 
 import net.wolfgangwerner.geo.distance.DistanceCalculationAlgorithm;
+import net.wolfgangwerner.geo.distance.GreatCircleDistanceAlgorithm;
 import net.wolfgangwerner.geo.distance.HaversineAlgorithm;
 
 public class GeoConstants {
 	public static double EARTH_RADIUS_IN_KM = 6371;
 	
 	public static DistanceCalculationAlgorithm HAVERSINE_ALGORITHM = new HaversineAlgorithm();
+	public static DistanceCalculationAlgorithm GREAT_CIRCLE_ALGORITHM = new GreatCircleDistanceAlgorithm();
 	
 	public static DistanceCalculationAlgorithm DEFAULT_ALGORITHM = HAVERSINE_ALGORITHM;
 }
