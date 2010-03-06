027437ccef4819b02776aef3cbbebf1375dac9e5
Wolfgang Werner <wolfgang.werner@gmail.com>
2010-03-06 14:36:18 +0100

    moved tests to separate fragment bundle

--- net.wolfgangwerner.geo/src/net/wolfgangwerner/geo/distance/DistanceCalculationAlgorithm.java
+++
@@ -1,7 +0,0 @@
-package net.wolfgangwerner.geo.distance;
-
-import net.wolfgangwerner.geo.model.GeoPoint;
-
-public interface DistanceCalculationAlgorithm {
-	public double distanceInKilometres(GeoPoint from, GeoPoint to);
-}
